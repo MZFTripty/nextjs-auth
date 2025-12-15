53dd6faaa98ccadb2a83d4bcb1ba97bc99820807
MZFTripty <zarinfatematripty@gmail.com>
2025-12-15 17:53:08 +0600

    fix: cast action to string in doSocialLogin function

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -4,7 +4,7 @@ import { signIn, signOut } from "@/auth";
 
 export async function doSocialLogin(formData: FormData) {
  const action = formData.get("action");
- await signIn(action, {
+ await signIn(action as string, {
     redirectTo: "/home"
  })
 }
