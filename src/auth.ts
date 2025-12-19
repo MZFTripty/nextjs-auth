c7a08090e931ad9a0725ed532c1ee36f7271067c
MZFTripty <zarinfatematripty@gmail.com>
2025-12-19 19:30:52 +0600

    refactor: clean up formatting and improve error handling in authentication logic

--- src/auth.ts
+++ src/auth.ts
@@ -5,59 +5,56 @@ import CredentialProvider from "next-auth/providers/credentials";
 import { getUserByEmail } from "./data/users";
 
 export const {
-    handlers:{GET, POST},
-    auth,
-    signIn,
-    signOut
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
 } = NextAuth({
-    session: {
-        strategy: "jwt"
-    },
-    providers: [
-        CredentialProvider({
-            async authorize(credentials){
-                if(credentials === null) return null;
-                try{
-                    const user = getUserByEmail(credentials?.email as string);
-                    if(user){
-                        const isMatch = user.password === credentials?.password;
-                        if(isMatch){
-                            return user;
-                        }
-                        else{
-                            throw new Error("Invalid password");
-                        }
-                    }
-                    else{
-                        throw new Error("User not found");
-                    }
-                }
-                catch(err){
-                    throw new Error("User not found")
-                }
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialProvider({
+      async authorize(credentials) {
+        if (credentials === null) return null;
+        try {
+          const user = getUserByEmail(credentials?.email as string);
+          if (user) {
+            const isMatch = user.password === credentials?.password;
+            if (isMatch) {
+              return user;
+            } else {
+              throw new Error("Invalid password");
             }
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            authorization: {
-                params: {
-                    prompt: "consent",
-                    access_type: "offline",
-                    response_type: "code"
-                }
-            }
-        }),
-        GitHubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            authorization: {
-                params: {
-                    prompt: "consent",
-                    access_type: "offline",
-                    response_type: "code"
-                }
-            }
-        })
-    ]
-})
+          } else {
+            throw new Error("User not found");
+          }
+        } catch (err) {
+          throw new Error("Authentication failed");
+        }
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+});
